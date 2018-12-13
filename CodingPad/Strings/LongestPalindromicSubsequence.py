5015a023f2e9abfe4d10d7a49ef90e68b1f79af9
Aditya Prabhu <adityalprabhu@gmail.com>
2018-12-13 12:05:18 -0500

    minor updates

--- CodingPad/Strings/LongestPalindromicSubsequence.py
+++ CodingPad/Strings/LongestPalindromicSubsequence.py
@@ -3,24 +3,24 @@
 def lps(str):
 
     # length of the string
-    strlen =  len(str)
+    n =  len(str)
 
     # create memoization table
-    L = [[0 for x in range(strlen)] for x in range(strlen)]
+    L = [[0 for x in range(n)] for x in range(n)]
 
     # all substrings of length 1 are palindromes and count is 1 character
-    for i in range(strlen):
+    for i in range(n):
         L[i][i] = 1
 
     # subsequence starts from 2 chars now
-    for sub in range(2, strlen+1):
+    for k in range(2, n+1):
         # subsequence of length 0 to of the right corners of the table
-        for i in range(strlen-sub+1):
+        for i in range(n-k+1):
 
-            j = i+sub-1
+            j = i+k-1
 
             # if two characters are the same then length is 2
-            if str[i] == str[j] and sub == 2:
+            if str[i] == str[j] and k == 2:
                 L[i][j] = 2
 
             #else remove both chars and check for the remaining string and add 2
@@ -32,11 +32,7 @@ def lps(str):
                 L[i][j] = max(L[i][j-1], L[i+1][j])
 
     # send value in the right most character, which is for the whole string
-    return L[0][strlen-1]
-
-
-    return 0
-
+    return L[0][n-1]
 
 
 if __name__ == '__main__':
